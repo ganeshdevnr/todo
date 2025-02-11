1081ee17712638c15e989ce416dd2c79041ee528
ganeshdevnr <ganesh.dev.nr@gmail.com>
2025-02-11 20:20:13 +0530

    apply styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import CssBaseline from "@mui/material/CssBaseline";
 import TaskList from "./components/TaskList";
 import TaskInput from "./components/TaskInput";
 import { useTasks } from "./hooks/useTasks";
+import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
+import "./App.css";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const darkTheme = createTheme({ palette: { mode: "dark" } });
 
@@ -19,13 +23,43 @@ export default function App() {
     <>
       <ThemeProvider theme={darkTheme}>
         <CssBaseline />
-        <TaskInput onAddTask={handleAddTask} />
-        <TaskList
-          tasks={tasks}
-          onToggleCompletion={handleToggleCompletion}
-          onDeleteTask={handleDeleteTask}
-          onDueDateChange={handleDueDateChangeTask}
-        />
+
+        <Box>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+              >
+                <MenuIcon />
+              </IconButton>
+
+              <Typography
+                className="text-center"
+                variant="h6"
+                component="div"
+                sx={{ flexGrow: 1 }}
+              >
+                MY TODO APP
+              </Typography>
+
+              <Button color="inherit">Login</Button>
+            </Toolbar>
+          </AppBar>
+
+          <div id="app-body">
+            <TaskInput onAddTask={handleAddTask} />
+            <TaskList
+              tasks={tasks}
+              onToggleCompletion={handleToggleCompletion}
+              onDeleteTask={handleDeleteTask}
+              onDueDateChange={handleDueDateChangeTask}
+            />
+          </div>
+        </Box>
       </ThemeProvider>
     </>
   );
